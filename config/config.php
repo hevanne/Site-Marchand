d0321908ec4e5c5f8b98d25f3c2fa90912cea279
salimkhr <salimkhr@gmaail.com>
2024-10-13 11:08:20 +0200

    fix config

--- config/config.php
+++ config/config.php
@@ -1,8 +1,7 @@
 <?php
-// config.php
-
 // Définition de constantes pour la configuration de la base de données
-define('DB_HOST', 'localhost');  // Hôte de la base de données (généralement localhost)
+define('DB_HOST', 'localhost');  // Hôte de la base de données
 define('DB_NAME', '');  // Nom de la base de données
 define('DB_USER', '');  // Nom d'utilisateur de la base de données
 define('DB_PASS', '');  // Mot de passe de la base de données
+define('DB_PORT', '5432');  // port de connection à la base de données
