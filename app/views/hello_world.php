17ae3036bed660a7651c2bfa7bcd2ff25d3c366f
Khraimeche Salim <salimkhraim@gmail.com>
2024-10-18 11:04:45 +0000

    Update hello_world.php

--- app/views/hello_world.php
+++ app/views/hello_world.php
@@ -1,3 +1,3 @@
 <?php require_once '../app/views/_template/header.php'; ?>
-        <h1>Hello <?php echo $name; ?></h1>
+        <h1>Hello <?= $name ?></h1>
 <?php require_once '../app/views/_template/footer.php'; ?>
